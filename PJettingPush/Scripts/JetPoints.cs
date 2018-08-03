9151fc0ac43cd4fb6c7832ac1feabae0c2a1b745
YuanSyun <rty3497374@gmail.com>
2018-08-03 15:48:24 +0800

    first commit

---
+++ PJettingPush/Scripts/JetPoints.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+	Coder: YuanSyun Ye (yuansyuntw@gmail.com)
+	Date: August, 3, 2018
+	Purpose: Define the point list of jetting.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace edu.nctu.nllab.PJettingPush
+{
+    public class JetPoints : MonoBehaviour
+    {
+		[SerializeField]
+        public List<JetPoint> listPoint;
+
+		[HideInInspector]
+		public float popingTime;
+
+		public List<JetPoint> GetSortListPoint{
+			get{
+				listPoint.Sort(CompareJetPoint);
+				return listPoint;
+			}
+		}
+
+		private int CompareJetPoint(JetPoint x, JetPoint y){
+			return x.enableTime.CompareTo(y.enableTime);
+		}
+    }
+}
+
---
+++ PJettingPush/Scripts/JetPrePoping.cs
@@ -0,0 +1,57 @@
+﻿
+/*
+	Coder: YuanSyun Ye (yuansyuntw@gmail.com)
+	Date: August, 3, 2018
+	Purpose: Pre-poping a JetPoints to JettingController.cs
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace edu.nctu.nllab.PJettingPush
+{
+    public class JetPrePoping : MonoBehaviour
+    {
+
+		#region public variable
+        public JettingController jController;
+		public JetPoints jPoints;
+		#endregion
+
+		#region public api
+		public void PrePoping(){
+			jPoints.popingTime = Time.time;
+			jController.AddJetPoints(jPoints);
+			//Debug.Log("PrePoing: " + transform.name);
+		}
+		#endregion
+
+		#region lifecycle
+        // Use this for initialization
+        void Start()
+        {
+			if(jController == null){
+				_CheckSetting("jController not seted");
+			}
+			if(jPoints == null){
+				_CheckSetting("JPoints not seted");
+			}
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+		#endregion
+
+		void _CheckSetting(string warning){
+			Debug.Log("JetPrePoing.cs - " + warning);
+			Destroy(this);
+		}
+    }
+
+}
+
